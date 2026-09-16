666b8b9b4eb2fdd2d63ab3667351e43eb5e3131f
bigeror <1836729@gmail.com>
2026-09-16 09:35:53 +0300

    Started implementing vectors

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ future usage simplicity. It is intendedd for personal use.
 
 ## Implemented content
 [X] Slice            (pointer to a fat pointer masked by a double pointer to datatype)
-[X] Vector           (pointer to a length & capacity prefixed array masked by a single pointer)
+[+/-] Vector           (pointer to a length & capacity prefixed array masked by a single pointer)
 [X] System           (general system manipulations like creating & reading files)
 [X] Build system     (replacement to a regular shell script purely in C)
--- misc.h
+++ misc.h
@@ -1,6 +1,42 @@
+// # define MISC_VECTOR // testing
+// # define MISC_VECTOR_IMPLEMENTATION // testing
+
 # ifndef MISC_LIBRARY_IMPORT
 # define MISC_LIBRARY_IMPORT
 
+# ifdef MISC_VECTOR
+# include <stddef.h>
 
+/// length, capacity, a, b, ...
+///                   ^ pointer
+
+typedef struct {
+    size_t count;
+    size_t capacity;
+} MISC_Vector_Header; // Datatype to prefix the vector
+
+/// Simple way to create stack based vector.
+/// Usage: `int* variable = vec_new(int, 1, 2, 3);`
+# define vec_new(type, ...) (type*)\
+((MISC_Vector_Header *)(&(struct {                                               \
+    size_t count;                                                                \
+    size_t capacity;                                                             \
+    type data[sizeof((type[]) {__VA_ARGS__}) / sizeof(type)];                    \
+}) {                                                                             \
+    .count = sizeof((type[]) {__VA_ARGS__}) / sizeof(type),                      \
+    .capacity = sizeof((type[]) {__VA_ARGS__}) / sizeof(type),                   \
+    .data = {__VA_ARGS__}                                                        \
+}) + 1)
 
 # endif
+
+# ifdef MISC_VECTOR_IMPLEMENTATION
+# undef MISC_VECTOR_IMPLEMENTATION
+
+# endif
+
+# endif
+
+// int main(void) {
+//     int* test = vec_new(int, 1 + 2, 2);
+// }
