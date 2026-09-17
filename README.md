3caae38981514676b8ece49e9b3897e7546f9ab5
bigeror <1836729@gmail.com>
2026-09-17 12:26:21 +0300

    Added more vector functions
    Fixed README

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ This is a small C library containing some small features for
 future usage simplicity. It is intendedd for personal use.
 
 ## Implemented content
-[X] Slice            (pointer to a fat pointer masked by a double pointer to datatype)
-[+/-] Vector           (pointer to a length & capacity prefixed array masked by a single pointer)
-[X] System           (general system manipulations like creating & reading files)
-[X] Build system     (replacement to a regular shell script purely in C)
+- [ ] Slice            (pointer to a fat pointer masked by a double pointer to datatype)
+- [ ] Vector           (pointer to a length & capacity prefixed array masked by a single pointer)
+- [ ] System           (general system manipulations like creating & reading files)
+- [ ] Build system     (replacement to a regular shell script purely in C)
--- misc.h
+++ misc.h
@@ -1,12 +1,20 @@
-// # define MISC_VECTOR // testing
-// # define MISC_VECTOR_IMPLEMENTATION // testing
+# if 0
+# define MISC_VECTOR // testing
+# define MISC_VECTOR_IMPLEMENTATION // testing
+# endif
 
 # ifndef MISC_LIBRARY_IMPORT
 # define MISC_LIBRARY_IMPORT
 
 # ifdef MISC_VECTOR
+
+# ifndef MISC_VECTOR_INITIAL_CAPACITY
+# define MISC_VECTOR_INITIAL_CAPACITY 10
+# endif
+
 # include <stddef.h>
-#include <stdlib.h>
+# include <stdlib.h>
+# include <assert.h>
 
 /// length, capacity, a, b, ...
 ///                   ^ pointer
@@ -30,16 +38,21 @@ typedef struct {
 }) + 1)
 
 # define vec_count(vector) (((MISC_Vector_Header*)(vector) - 1) -> count)
+# define vec_capacity(vector) (((MISC_Vector_Header*)(vector) - 1) -> capacity)
 
-/// Allocates the vector with specified size
-/// NOTICE: vectors allocated by it MUST be destroyed using vec_destroy
-static inline void* vec_alloc(size_t sizeof_value, size_t capacity) {
-    MISC_Vector_Header* header = malloc(sizeof(MISC_Vector_Header) + sizeof_value * capacity);
-    if (header == NULL) return NULL;
-    header -> count = 0;
-    header -> capacity = capacity;
-    return header + 1;
-}
+void* vec_alloc(size_t sizeof_value, size_t capacity);
+void* vec_realloc(size_t sizeof_value, size_t new_capacity, void* vector);
+
+# define vec_push(vector, value) do {                                                                      \
+    if ((vector) == NULL) (vector) = vec_alloc(sizeof(vector[0]), MISC_VECTOR_INITIAL_CAPACITY);           \
+    if (vec_capacity(vector) <= vec_count(vector))                                                         \
+        vec_realloc(sizeof(vector[0]), vec_count(vector) * 2 + MISC_VECTOR_INITIAL_CAPACITY, (vector));    \
+    if ((vector) == NULL) assert(0 && "Couldn't allocate vector before pushing to it.");                   \
+    (vector)[vec_count(vector)] = value;                                                                   \
+    vec_count(vector) += 1;                                                                                \
+} while (0)
+
+// vec_from_array(type, array, count) vec_alloc(sizeof(type), size_t capacity)
 
 /// All the vectors made in the way besides vec_new must be destroyed,
 /// but due to their internal complexity regular free() won't work
@@ -50,18 +63,49 @@ static inline void* vec_alloc(size_t sizeof_value, size_t capacity) {
 # ifdef MISC_VECTOR_IMPLEMENTATION
 # undef MISC_VECTOR_IMPLEMENTATION
 
-# endif
+/// Allocates the vector with specified size
+/// NOTICE: vectors allocated by it MUST be destroyed using vec_destroy
+void* vec_alloc(size_t sizeof_value, size_t capacity) {
+    MISC_Vector_Header* header = malloc(sizeof(MISC_Vector_Header) + sizeof_value * capacity);
+    if (header == NULL) return NULL;
+    header -> count = 0;
+    header -> capacity = capacity;
+    return header + 1;
+}
+
+/// Reallocates the vector with specified size
+void* vec_realloc(size_t sizeof_value, size_t new_capacity, void* vector) {
+    MISC_Vector_Header* header = (MISC_Vector_Header*)vector - 1;
+    MISC_Vector_Header* new_vec_header = realloc(header, sizeof(MISC_Vector_Header) + sizeof_value * new_capacity);
+    if (new_vec_header == NULL) return NULL;
+    new_vec_header -> capacity = new_capacity;
+    return new_vec_header + 1;
+}
 
 # endif
 
-// int main(void) {
-// #include <stdio.h>
-//     int* test = vec_new(int, 1 + 2, 2, 5);
-//     printf("%lu\n", vec_count(test));
-//     vec_count(test) = 1;
-//     printf("%lu\n", vec_count(test));
-//
-//     int* test2 = vec_alloc(sizeof(int), 5);
-//     printf("%lu\n", vec_count(test2));
-//     vec_destroy(test2);
-// }
+# endif
+
+# if 0
+int main(void) {
+#include <stdio.h>
+    int* test = vec_new(int, 1 + 2, 2, 5);
+    printf("%lu\n", vec_count(test));
+    vec_count(test) = 1;
+    printf("%lu\n", vec_count(test));
+    assert(0 && "test");
+
+    int* test2 = vec_alloc(sizeof(int), 5);
+    printf("%lu / %lu\n", vec_count(test2), vec_capacity(test2));
+    vec_push(test2, 10);
+    vec_push(test2, 11);
+    printf("%i\n", test2[0]);
+    printf("%i\n", test2[1]);
+    int* test3 = vec_realloc(sizeof(int), 10, test2);
+    printf("%lu / %lu\n", vec_count(test3), vec_capacity(test3));
+    printf("%i\n", test3[0]);
+    char* test4 = NULL;
+    // vec_destroy(test2);
+    vec_destroy(test3);
+}
+# endif
